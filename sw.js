8cffc92a610fb3a89cd2163164cd50b6ff575c1f
TheWorstCoderZZZ <97879732+TheWorstCoderZZZ@users.noreply.github.com>
2026-03-05 23:18:30 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'party-cal-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝 Service Worker 並快取基本檔案
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截網絡請求，提升載入速度
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
